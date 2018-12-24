64c6d1bd317d2c25ddcd99cd2446577c1777eb2f
Donny M <Mobslayer205@gmail.com>
2018-12-24 18:29:10 -0500

    Basic raycast setup

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -14,6 +14,7 @@ public class GunController : MonoBehaviour {
     [Header("Gun Stats")]
     [SerializeField] float bulletSpeed = 10f;
     [SerializeField] float timeBetweenFire = 0.5f;
+    [SerializeField] float bulletLifetime = 5f;
 
     bool canFire = true;
 
@@ -32,6 +33,7 @@ public class GunController : MonoBehaviour {
         if(!canFire) { return; }
         if (Input.GetMouseButton(0))
         {
+            ShootRay();
             ShootBullet();
             FireParticles();
             PlayShootSound();
@@ -47,11 +49,26 @@ public class GunController : MonoBehaviour {
     {
         gunAudio.Play();
     }
-
+    private void ShootRay()
+    {
+        RaycastHit hit;
+        Debug.Log(transform.position);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Hit something");
+            Destroy(hit.collider.gameObject);
+        }
+        else
+        {
+            Debug.Log("Did not hit something");
+            Debug.DrawRay(transform.position, transform.forward * 200, Color.white, 200);
+        }
+    }
     private void ShootBullet()
     {
         var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
         bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.forward * bulletSpeed;
+        Destroy(bullet, bulletLifetime);
     }
     private void FireParticles()
     {
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    GunController gun;
+	// Use this for initialization
+	void Start () {
+        gun = GetComponentInChildren<GunController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    void ProcessFire()
+    {
+        if(Input.)
+    }
+}
