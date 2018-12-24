a0a5c0e32a2fa6c4c3771bdc9b7b71a63115ef94
Donny M <Mobslayer205@gmail.com>
2018-12-23 23:15:15 -0500

    Basic shooting on button press

---
+++ Assets/GunController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    [Header("Bullet")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawn;
+    [SerializeField] float bulletSpeed = 10f;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Fire();
+	}
+    void Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            Debug.Log(bulletSpawn.position.x + ", " +  bulletSpawn.position.y + ", " + bulletSpawn.position.z);
+            var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.forward * bulletSpeed;
+        }
+    }
+}
